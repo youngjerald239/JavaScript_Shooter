0d15379eb6758543b25365a20dd94b898a2fe1e6
youngjerald239 <youngjerald239@gmail.com>
2022-09-01 18:49:50 -0500

    added score to canvas

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight 
+let score = 0
+ctx.font = '50px Impact'
 
 let timeToNextRaven = 0
 let ravenInterval = 500
@@ -25,22 +27,33 @@ class Raven {
         this.frame = 0
         this.maxFrame = 4
         this.timeSinceFlap = 0
-        this.flapInterval = 100
+        this.flapInterval = Math.random() * 50 + 50
     }
-    
-    update(deltatime) {
-        this.x -= this.directionX
+
+    update(deltatime){
+        if (this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1
+        }
+        this.x -= this.directionX 
+        this.y -= this.directionY
         if (this.x < 0 - this.width) this.markedForDeletion = true 
         this.timeSinceFlap += deltatime 
         if (this.timeSinceFlap > this.flapInterval) {
             if (this.frame > this.maxFrame) this.frame = 0
             else this.frame++
+            this.timeSinceFlap = 0
         }
     }
     draw() {
         ctx.strokeRect(this.x, this.y, this.width, this.height)
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
+}
+
+function drawScore(){
+    ctx.fillStyle = 'white'
+    ctx.fillText('Score:' + score, 50, 75)
+    ctx.fillText('Score:' + score, 50, 75)
 }
 
 function animate(timestamp) {
@@ -52,6 +65,7 @@ function animate(timestamp) {
         ravens.push(new Raven())
         timeToNextRaven = 0 
     }
+    drawScore();
     [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion)
