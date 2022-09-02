c5845c2b5a2d4dbd178252457540ff356c436799
youngjerald239 <youngjerald239@gmail.com>
2022-09-01 22:19:39 -0500

    complete particle

--- script.js
+++ script.js
@@ -53,7 +53,10 @@ class Raven {
             else this.frame++
             this.timeSinceFlap = 0
             if(this.hasTrail){
-                particles.push(new Particle(this.x, this.y, this.width, this.color))
+                for (let i = 0; i < 5; i++){
+                    particles.push(new Particle(this.x, this.y, this.width, this.color))
+                }
+                
             }
 
         }
@@ -100,8 +103,8 @@ let particles = []
 class Particle {
     constructor(x, y, size, color) {
         this.size = size
-        this.x = x + this.size/2
-        this.y = y + this.size/3
+        this.x = x + this.size/2 + Math.random() * 50 - 25
+        this.y = y + this.size/3 + Math.random() * 50 - 25
         this.radius = Math.random() * this.size/10
         this.maxRadius = Math.random() * 20 + 35
         this.markedForDeletion = false
@@ -111,15 +114,17 @@ class Particle {
     update(){
         this.x += this.speedX
         this.radius += 0.5
-        if (this.radius > this.maxRadius) this.markedForDeletion = true
+        if (this.radius > this.maxRadius - 5) this.markedForDeletion = true
 
     }
     draw(){
-        ctx.globalAlpha = 1
+        ctx.save()
+        ctx.globalAlpha = 1 - this.radius/this.maxRadius
         ctx.beginPath()
         ctx.fillStyle = this.color
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
         ctx.fill()
+        ctx.restore()
     }
 }
 
